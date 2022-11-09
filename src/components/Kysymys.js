e6f0c89496e5522eb7cfdd776bf87dd4a334b8fd
tommanni <108432153+tommanni@users.noreply.github.com>
2022-11-09 18:15:43 +0200

    Add files via upload

--- src/components/Kysymys.js
+++ src/components/Kysymys.js
@@ -4,100 +4,147 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { useContext } from 'react';
 import { TenttiContext } from '../App';
+import axios from 'axios';
 
 
 
 const Kysymys = ({ kysymys, kysymysNimi, tenttiId, kysymysIndex }) => {
     const tenttiContext = useContext(TenttiContext)
 
+    async function poistaKysymys() {
+        try {
+            await axios.delete('http://localhost:8080/poista-kysymys', { data: { tenttiId: tenttiId, kysymys: kysymys.kysymys, userId: tenttiContext.tenttiDatat.kayttaja.id } })
+            console.log(kysymys, tenttiId, tenttiContext.tenttiDatat.kayttaja.id)
+            tenttiContext.dispatch({
+                type: 'POISTA_KYSYMYS',
+                payload: {
+                    tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                    kysymys: kysymys.kysymys,
+                    tenttiId: tenttiId
+                }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const poistaVastaus = async (vastaus) => {
+        await axios.delete('http://localhost:8080/poista-vastaus', { data: { kysymysId: kysymys.id, vastaus: vastaus, userId: tenttiContext.tenttiDatat.kayttaja.id } })
+        tenttiContext.dispatch({
+            type: 'POISTA_VASTAUS',
+            payload: {
+                tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                tenttiId: tenttiId,
+                kysymysId: kysymys.id,
+                kysymysIndex: kysymysIndex,
+                vastaus: vastaus
+            }
+        })
+    }
+
+    const lisaaVastaus = async () => {
+        await axios.post('http://localhost:8080/lisaa-vastaus', { kysymysId: kysymys.id })
+        tenttiContext.dispatch({
+            type: 'LISAA_VASTAUS',
+            payload: {
+                tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                kysymysIndex: kysymysIndex,
+                kysymysId: kysymys.id,
+                id: kysymys.vastaukset.length + 1
+            }
+        })
+    }
+
+    const vaihdaOikein = async (vastausId, oikein, vastausIndex) => {
+        await axios.put('http://localhost:8080/vastaus-oikein', { vastausId: vastausId, oikein: oikein })
+        tenttiContext.dispatch({
+            type: 'KYSYMYS_OIKEIN',
+            payload: {
+                tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                vastausId: vastausId,
+                kysymysIndex: kysymysIndex,
+                vastausIndex: vastausIndex
+            }
+        })
+    }
+
     return (
         <div className='kysymys'>
             <p className='kysymysTeksti'><b>{kysymysNimi}</b>{tenttiContext.kayttaja === 1 && <input type="text" placeholder=' vaihda kysymys' onChange={(event) => {
-                tenttiContext.dispatch({
-                    type: "KYSYMYKSEN_NIMI_MUUTTUI",
-                    payload: {
-                        nimi: event.target.value,
-                        tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                        tenttiId: tenttiId,
-                        kysymysIndex: kysymysIndex,
-                        kysymysId: kysymys.id
-                    }
-                })
-            }} />}
-                {tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<DeleteIcon />} className='poista-kysymys' onClick={() => tenttiContext.dispatch({
-                    type: 'POISTA_KYSYMYS',
-                    payload: {
-                        tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                        kysymys: kysymys.kysymys,
-                        tenttiId: tenttiId
-                    }
-                })} >
-
-                </Button>}
-            </p>
-            {kysymys.vastaukset.map((vastaus, index) =>
-                <div key={vastaus.id} className='vastaus'>
-
-                    {tenttiContext.kayttaja === -1 && tenttiContext.vastaukset === 1 ? <Checkbox color="default" checked={!vastaus.valinta} disableRipple /> : tenttiContext.kayttaja === -1 && <Checkbox color="default" onClick={() => tenttiContext.dispatch({
-                        type: 'ASETA_VALINTA',
-                        payload: {
-                            tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                            kysymysIndex: kysymysIndex,
-                            vastausIndex: index
-                        }
-                    })} />}
-
-                    {tenttiContext.vastaukset === 1 && vastaus.oikein === true && tenttiContext.kayttaja === -1 ? <Checkbox color="default" checked={true} disableRipple />
-                        : tenttiContext.vastaukset === 1 && vastaus.oikein === false && tenttiContext.kayttaja === -1 ? <Checkbox checked={false} disableRipple color='success' />
-                            : tenttiContext.kayttaja === 1 ? <Checkbox color="default" defaultChecked={vastaus.oikein} onClick={() => tenttiContext.dispatch({
-                                type: 'KYSYMYS_OIKEIN',
-                                payload: {
-                                    tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                                    vastausId: vastaus.id,
-                                    kysymysIndex: kysymysIndex,
-                                    vastausIndex: index
-                                }
-                            })}
-                            /> : ""}
-                    {vastaus.vastaus}
-                    {tenttiContext.kayttaja === 1 && <input placeholder=' vaihda vastaus' onChange={(event) => {
+                try {
+                    const muutaKysymyksenNimi = async (tenttiId, kysymysId, nimi) => {
+                        await axios.put('http://localhost:8080/kysymyksen-nimi-muuttui', { tenttiId: tenttiId, kysymysId: kysymysId, nimi: nimi })
                         tenttiContext.dispatch({
-                            type: "VASTAUKSEN_NIMI_MUUTTUI",
+                            type: "KYSYMYKSEN_NIMI_MUUTTUI",
                             payload: {
                                 nimi: event.target.value,
                                 tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                                tenttiId: tenttiId,
                                 kysymysIndex: kysymysIndex,
-                                vastausIndex: index,
-                                kysymysId: kysymys.id,
-                                vastausId: vastaus.id
+                                kysymysId: kysymys.id
                             }
                         })
-                    }} />}
-
-                    {tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<DeleteIcon />} className='poista-vastaus' onClick={() => tenttiContext.dispatch({
-                        type: 'POISTA_VASTAUS',
-                        payload: {
-                            tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                            tenttiId: tenttiId,
-                            kysymysId: kysymys.id,
-                            kysymysIndex: kysymysIndex,
-                            vastaus: vastaus.vastaus
-                        }
-                    })} ></Button>}
-                </div>
-            )}
-
-            {tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<AddCircleIcon />} className='lisaa-vastaus' onClick={() => tenttiContext.dispatch({
-                type: 'LISAA_VASTAUS',
-                payload: {
-                    tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
-                    kysymysIndex: kysymysIndex,
-                    kysymysId: kysymys.id,
-                    id: kysymys.vastaukset.length + 1
+                    }
+                    muutaKysymyksenNimi(tenttiId, kysymys.id, event.target.value)
+                } catch (err) {
+                    console.log(err)
                 }
-            })}>LISÄÄ VASTAUS</Button>}
+            }} />}
+                {tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<DeleteIcon />} className='poista-kysymys' onClick={() => poistaKysymys()} >
 
-        </div>
+                </Button>}
+            </p>
+            {
+                kysymys.vastaukset.map((vastaus, index) =>
+                    <div key={vastaus.id} className='vastaus'>
+
+                        {tenttiContext.kayttaja === -1 && tenttiContext.vastaukset === 1 ? <Checkbox color="default" checked={!vastaus.valinta} disableRipple /> : tenttiContext.kayttaja === -1 && <Checkbox color="default" onClick={() => tenttiContext.dispatch({
+                            type: 'ASETA_VALINTA',
+                            payload: {
+                                tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                                kysymysIndex: kysymysIndex,
+                                vastausIndex: index
+                            }
+                        })} />}
+
+                        {tenttiContext.vastaukset === 1 && vastaus.oikein === true && tenttiContext.kayttaja === -1 ? <Checkbox color="default" checked={true} disableRipple />
+                            : tenttiContext.vastaukset === 1 && vastaus.oikein === false && tenttiContext.kayttaja === -1 ? <Checkbox checked={false} disableRipple color='success' />
+                                : tenttiContext.kayttaja === 1 ? <Checkbox color="default" defaultChecked={vastaus.oikein} onClick={() => vaihdaOikein(vastaus.id, vastaus.oikein, index)}
+                                /> : ""}
+                        {vastaus.vastaus}
+                        {tenttiContext.kayttaja === 1 && <input placeholder=' vaihda vastaus' onChange={(event) => {
+                            try {
+                                const muutaVastauksenNimi = async (kysymysId, vastausId, nimi) => {
+                                    await axios.put('http://localhost:8080/vastauksen-nimi-muuttui', { kysymysId: kysymysId, vastausId: vastausId, nimi: nimi })
+                                    tenttiContext.dispatch({
+                                        type: "VASTAUKSEN_NIMI_MUUTTUI",
+                                        payload: {
+                                            nimi: event.target.value,
+                                            tenttiIndex: tenttiContext.tentit.findIndex(tentti => tentti.id === tenttiId),
+                                            kysymysIndex: kysymysIndex,
+                                            vastausIndex: index,
+                                            kysymysId: kysymys.id,
+                                            vastausId: vastaus.id
+                                        }
+                                    })
+                                }
+                                muutaVastauksenNimi(kysymys.id, vastaus.id, event.target.value)
+                            } catch (err) {
+                                console.log(err)
+                            }
+
+                        }} />}
+
+                        {tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<DeleteIcon />} className='poista-vastaus' onClick={() => poistaVastaus(vastaus.vastaus)} ></Button>}
+                    </div>
+                )
+            }
+
+            {
+                tenttiContext.kayttaja === 1 && <Button style={{ color: '#fff' }} startIcon={<AddCircleIcon />} className='lisaa-vastaus' onClick={() => lisaaVastaus()}>LISÄÄ VASTAUS</Button>
+            }
+
+        </div >
 
     )
 }
--- src/components/Tentit.js
+++ src/components/Tentit.js
@@ -4,9 +4,36 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useContext } from 'react';
 import { TenttiContext } from '../App';
+import axios from 'axios';
 
 const Tentit = () => {
     const tenttiContext = useContext(TenttiContext)
+
+    async function poistaTentti(nimi, tenttiId) {
+        try {
+            await axios.delete('http://localhost:8080/poista-tentti', { data: { tenttiId: tenttiId } })
+            tenttiContext.dispatch({
+                type: 'POISTA_TENTTI',
+                payload: { nimi: nimi, tenttiId: tenttiId, setToValue: tenttiContext.setToValue }
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const lisaaTentti = async () => {
+        await axios.post('http://localhost:8080/lisaa-tentti')
+        tenttiContext.dispatch({
+            type: 'LISAA_TENTTI'
+        })
+    }
+
+    const lisaaKysymys = async () => {
+        await axios.post('http://localhost:8080/lisaa-kysymys', { tenttiIndex: tenttiContext.value[0].id })
+        tenttiContext.dispatch({
+            type: 'LISAA_KYSYMYS',
+            payload: tenttiContext.tentit.findIndex(tentti1 => tentti1.id === tenttiContext.value[0].id)
+        })
+    }
 
     return (
         <div className='tentit'>
@@ -15,25 +42,29 @@ const Tentit = () => {
                     <Button style={{ color: '#fff' }} key={tentti.id}
                         onClick={() => tenttiContext.setToValue(tentti.id)}>{tentti.nimi}</Button>
                     {tenttiContext.kayttaja === 1 ? <input key={tentti.id} placeholder=' vaihda tentin nimi' type="text" onChange={(event) => {
-                        tenttiContext.dispatch({
-                            type: "TENTIN_NIMI_MUUTTUI",
-                            payload: {
-                                nimi: event.target.value,
-                                tentinIndex: tenttiContext.tentit.findIndex(tentti1 => tentti1.id === tentti.id),
-                                tentinId: tentti.id
+                        const muutaTentinNimi = async (tenttiId, nimi) => {
+                            try {
+                                await axios.put('http://localhost:8080/tentin-nimi-muuttui', { tenttiId: tenttiId, nimi: nimi })
+                                tenttiContext.dispatch({
+                                    type: "TENTIN_NIMI_MUUTTUI",
+                                    payload: {
+                                        nimi: event.target.value,
+                                        tentinIndex: tenttiContext.tentit.findIndex(tentti1 => tentti1.id === tentti.id),
+                                        tentinId: tentti.id
+                                    }
+                                })
+                            } catch (err) {
+                                console.log(err)
                             }
-                        })
+                        }
+                        muutaTentinNimi(tentti.id, event.target.value)
+
                     }} /> : ""}
-                    {tenttiContext.kayttaja === 1 ? <Button key={tentti.id} style={{ color: '#fff' }} startIcon={<DeleteIcon />} onClick={() => tenttiContext.dispatch({
-                        type: 'POISTA_TENTTI',
-                        payload: { nimi: tentti.nimi, tenttiId: tentti.id, setValue: tenttiContext.setValue }
-                    })} /> : ""}
+                    {tenttiContext.kayttaja === 1 ? <Button key={tentti.id} style={{ color: '#fff' }} startIcon={<DeleteIcon />} onClick={() => poistaTentti(tentti.nimi, tentti.id)} /> : ""}
                 </>
             )
             }
-            {tenttiContext.kayttaja === 1 && <Button startIcon={<AddCircleIcon />} style={{ color: '#fff' }} onClick={() => tenttiContext.dispatch({
-                type: 'LISAA_TENTTI'
-            })}>LISÄÄ TENTTI</Button>}
+            {tenttiContext.kayttaja === 1 && <Button startIcon={<AddCircleIcon />} style={{ color: '#fff' }} onClick={() => lisaaTentti()}>LISÄÄ TENTTI</Button>}
             {Object.values(tenttiContext.value).length !== 0 ?
 
                 tenttiContext.value.map(tentti => <Tentti
@@ -45,10 +76,7 @@ const Tentit = () => {
             {tenttiContext.kayttaja === -1 ? <Button
                 style={{ color: '#fff' }}
                 onClick={tenttiContext.oikeatVastaukset}>NÄYTÄ VASTAUKSET
-            </Button> : Object.values(tenttiContext.value).length !== 0 && <Button startIcon={<AddCircleIcon />} style={{ color: '#fff' }} onClick={() => tenttiContext.dispatch({
-                type: 'LISAA_KYSYMYS',
-                payload: tenttiContext.tentit.findIndex(tentti1 => tentti1.id === tenttiContext.value[0].id)
-            })}>LISÄÄ KYSYMYS</Button>}
+            </Button> : Object.values(tenttiContext.value).length !== 0 && <Button startIcon={<AddCircleIcon />} style={{ color: '#fff' }} onClick={() => lisaaKysymys()}>LISÄÄ KYSYMYS</Button>}
         </div>
     )
 }
